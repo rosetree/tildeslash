8b0b11c6440974703cee96089e2db4ba030e3b7a
Micha Rosenbaum <micha@rosetree.de>
2014-07-21 19:17:01 +0200

    add `setup.sh`
    - Remove files that will be replaced with this repository
    - Link all config files
    - Create folders that are needed from files in this repo
    - Run the configuration setup in `.private`

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -f ~/.bashrc
+rm -f ~/.bash_aliases
+rm -f ~/.gitexcludes
+rm -f ~/.gitconfig
+rm -f ~/.signature
+rm -f ~/.vimrc
+rm -f ~/.config/lxsession/LXDE/autostart
+rm -rf ~/.i3
+rm -rf ~/bin
+rm -rf ~/.vim
+
+ln -s ~/.tildeslash/.bashrc ~/.bashrc
+ln -s ~/.tildeslash/.bash_aliases ~/.bash_aliases
+ln -s ~/.tildeslash/.gitexcludes ~/.gitexcludes
+ln -s ~/.tildeslash/.gitconfig ~/.gitconfig
+ln -s ~/.tildeslash/.signature ~/.signature
+ln -s ~/.tildeslash/.vimrc ~/.vimrc
+ln -s ~/.tildeslash/.LXDE_autostart ~/.config/lxsession/LXDE/autostart
+ln -s ~/.tildeslash/.i3 ~/.i3
+ln -s ~/.tildeslash/bin ~/bin
+ln -s ~/.tildeslash/.vim ~/.vim
+
+# Directory used to store temporary vim files.
+mkdir -p ~/tmp/.vim/swap
+
+source "./.private/setup.sh"
